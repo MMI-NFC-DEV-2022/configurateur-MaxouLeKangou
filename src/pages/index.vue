3a4e684663a88e37f3caeffb4c762a220cf5eecb
Maxime LEGRAND <maxime.legrand111@outlook.com>
2024-03-05 15:30:48 +0100

    feat. Liste d'exemples

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -3,16 +3,29 @@
 import { RouterLink } from "vue-router";
 import BasketProfil from "../components/BasketProfil.vue";
 import type { Basket } from "../types";
-const exemple0: Basket = {
-  semelle: "#FF0000",
-  empeigne: "#FFFFFF",
-  pointe: "#FFFFFF",
-  oeillet: "#FFFFFF",
-  bande: "#00FF00",
-  languette: "#FFFFFF",
-  lacet: "#00FF00",
-  trimestre: "#FFFFFF",
-};
+
+const exemples: Basket[] = [
+	{
+		semelle: "#FF0000",
+		empeigne: "#FFFFFF",
+		pointe: "#FFFFFF",
+		oeillet: "#FFFFFF",
+		bande: "#00FF00",
+		languette: "#FFFFFF",
+		lacet: "#00FF00",
+		trimestre: "#FFFFFF",
+	},
+	{
+		semelle: "#0000FF",
+		empeigne: "#FFFF00",
+		pointe: "#FFFF00",
+		oeillet: "#FFFF00",
+		bande: "#FF00FF",
+		languette: "#FFFF00",
+		lacet: "#FF00FF",
+		trimestre: "#FFFF00",
+	},
+];
 </script>
 <template>
   <section>
@@ -20,7 +33,7 @@ const exemple0: Basket = {
     <div class="flex flex-wrap gap-2">
       <div class="w-64">
         <!-- <RouterLink :to="{ name: 'basket-exemple-data', params: { data: JSON.stringify(exemple0) },}"> -->
-          <BasketProfil class="w-64" v-bind="exemple0" />
+          <BasketProfil class="w-64" v-for="basket in exemples" :key="basket.bande" v-bind="basket" />
         <!-- </RouterLink> -->
       </div>
     </div>
