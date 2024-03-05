03e1d501ebcf72c5db0fd76708b296db05d8d087
Maxime LEGRAND <maxime.legrand111@outlook.com>
2024-03-05 11:02:47 +0100

    feat. Create project

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { RouterLink, RouterView, } from 'vue-router'
+import { supabase, signInWithGithub } from './supabase'
+
+
+
+</script>
+
+<template>
+    
+  <Suspence>
+    <RouterView />
+  </Suspence>
+</template>
---
+++ src/types.ts
@@ -0,0 +1,11 @@
+export interface SchemaOffreMaison {
+    id?: string; 
+    name: string;
+    price: number;
+    favorie: boolean;
+    picture: string;
+    bedroom: number;
+    bathroom: number;
+    address: string;
+    surface: string;
+}
