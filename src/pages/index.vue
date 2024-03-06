e3fbc6e7a8108716e8fbdbfa28e85803b587e639
Maxime LEGRAND <maxime.legrand111@outlook.com>
2024-03-06 15:17:00 +0100

    feat. Récupération des données depuis supabase

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,13 +1,37 @@
+<!-- eslint-disable vue/multi-word-component-names -->
 <script setup lang="ts">
-import FormBasket from '@/components/FormBasket.vue';
-import FormImages from '@/components/FormImages.vue';
-import { RouterLink } from 'vue-router';
+import BasketProfil from "@/components/SvgProfil.vue";
+import type { Basket } from "@/types";
+import { supabase } from "@/supabase";
+import { ref } from "vue";
+
+const baskets = ref<Basket[]>([]);
+
+async function fetchBaskets() {
+  const { data, error } = await supabase.from('basket').select();
+  if (error) {
+    console.error("Erreur lors de la récupération des paniers depuis Supabase:", error.message);
+    return;
+  }
+  baskets.value = data || [];
+}
+
+fetchBaskets();
+
+function getDataString(index: number): string {
+  return encodeURIComponent(JSON.stringify(baskets.value[index]));
+}
 </script>
 
 <template>
-  <RouterLink to="/BasketIndex">navigation</RouterLink>
-  <main>bonjour de home</main>
-  <FormBasket/>
-  <FormImages/>
-
+  <section>
+    <h1 class="text-2xl">Liste des Baskets de la BDD</h1>
+    <div class="flex flex-wrap gap-2">
+      <div v-for="(basket, index) in baskets" :key="index" class="w-64">
+        <router-link :to="`/basket/exemple/${getDataString(index)}`">
+          <BasketProfil class="w-64" v-bind="basket" />
+        </router-link>
+      </div>
+    </div>
+  </section>
 </template>
--- src/supabase.ts
+++ src/supabase.ts
@@ -1,12 +1,16 @@
 import { createClient } from '@supabase/supabase-js'
-import {ref} from '@vue/reactivity'
+import { ref } from 'vue'
+
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
-console.log("affiche .env : ",supabaseUrl, supabaseAnonKey);
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
 
-globalThis.supabase = supabase
+export async function signInWithGithub() {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+        provider: 'github',
+    })
+}
 
 const { data } = await supabase.auth.getUser()
 export const user = ref(data.user)
